bb597bb87fff3a33ac6e84e0e79d8edeaa4a2ed1
Mohit Sharma <2015ucp1511@mnit.ac.in>
2018-09-28 21:26:58 +0530

    Ask.html

--- hack/settings.py
+++ hack/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'crispy_forms',
     'home',
     'Profile',
     'django.contrib.admin',
--- home/forms.py
+++ home/forms.py
@@ -4,8 +4,8 @@ from django.forms import ModelForm
 
 
 class LoginForm(forms.Form):
-	Username = forms.CharField()
-	Password = forms.CharField(widget = forms.PasswordInput)
+	Username = forms.CharField(label = "Username", widget = forms.TextInput(attrs = {'class':'form-control'}))
+	Password = forms.CharField(label = "Password",widget = forms.PasswordInput(attrs = {'class':'form-control'}))
 	
 	
 class RegisterForm(ModelForm):
@@ -15,14 +15,15 @@ class RegisterForm(ModelForm):
 
 
 class QuestionForm(forms.Form):
-	text = forms.CharField(max_length = 140)
-	explaination = forms.CharField(max_length = 300)
+	text = forms.CharField(label = "Text",max_length = 140 , widget = forms.TextInput(attrs = {'class':'form-control'}) )
+	explanation = forms.CharField(label = "Explanation",max_length = 1000 , widget = forms.Textarea(attrs = {'class':'form-control'}))
 	topic_choices = (
 		('topic1' , 'topic1'),
 		('topic2' , 'topic2'),
 		)
-	interests = forms.MultipleChoiceField(required = True , widget = forms.CheckboxSelectMultiple , choices = topic_choices)
+	interests = forms.MultipleChoiceField(label = "Tags",required = True , widget = forms.CheckboxSelectMultiple(attrs = {'class':'form-check-inline'}) 
+		, choices = topic_choices)
 
 class AnswerForm(forms.Form):
-	text = forms.CharField(max_length = 300)
+	text = forms.CharField(max_length = 300 , widget = forms.TextInput(attrs = {'class':'form-control'}) )
 	
